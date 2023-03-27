f66484bcc7adf836c9fbaf035ff70b362c0d91cd
jovkin27 <118188660+jovkin27@users.noreply.github.com>
2023-03-27 22:02:09 +0300

    Звучёк

---
+++ Snake/Parametrs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Parametrs
+    {
+        private string resourcesFolder;
+
+        public Parametrs()
+        {
+            var ind = Directory.GetCurrentDirectory().ToString()
+                .IndexOf("bin", StringComparison.Ordinal); // Получить индекс папки bin
+
+            string binFolder =
+                Directory.GetCurrentDirectory().ToString().Substring(0, ind)
+                    .ToString(); // путь до указанной в инкесе папки
+
+            resourcesFolder = binFolder + "resources\\";
+        }
+
+        public string GetResourceFolder()
+        {
+            return resourcesFolder;
+        }
+    }
+}
---
+++ Snake/Sounds.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WMPLib;
+
+namespace Snake
+{
+    public class Sounds
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        private string pathToMedia;
+
+        public Sounds(string pathToResources)
+        {
+
+            pathToMedia = pathToResources;
+        }
+
+        public void Play(string sound)
+        {
+            player.URL = pathToMedia + sound;// "stardust.mp3";
+            player.settings.volume = 30;
+            player.controls.play();
+            player.settings.setMode("loop", true); // loop mode
+        }
+        public void Stop(string sound)
+        {
+            player.URL = pathToMedia + "lost.mp3";
+            player.controls.stop();
+        }
+        public void PlayEat()
+        {
+            player.URL = pathToMedia + "click.mp3";
+            player.settings.volume = 100;
+            player.controls.play();
+        }
+    }
+}
