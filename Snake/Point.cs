1504c28658b5bee0efa8ac43655c07595e7051e3
jovkin27 <118188660+jovkin27@users.noreply.github.com>
2023-03-27 20:47:24 +0300

    Змей исправлен

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -71,11 +71,5 @@ namespace Snake
             return x + "," + y + "," + sym;
         }
 
-        internal bool IsHit(Point p)
-        {
-            return p.x == this.x && p.y == this.y;
-        }
-
-
     }
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -30,57 +30,7 @@ namespace Snake
             Point p = new Point(4, 5, '*', ConsoleColor.Red);
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            FoodCreator foodCreator = new FoodCreator(101, 26, '¤', ConsoleColor.Green);
-            Point food = foodCreator.CreateFood();
-            food.Draw();
-            Score score = new Score(0, 1);//score =0, level=1
-            score.speed = 400;
-            score.ScoreWrite();
-            while (true)
-            {
-                if (snake.Eat(food))
-                {
-                    //soundeat.Play("lost.mp3");
-                    score.ScoreUp();
-                    score.ScoreWrite();
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                    //sound.Stop("stardust.mp3");
-                    if (score.ScoreUp())
-                    {
-                        score.speed -= 10;
-                    }
-                }
-                else
-                {
-                    snake.Move();
-                }
-                Thread.Sleep(score.speed);
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-                    snake.HandleKey(key.Key);
-                }
-            }
-        }
-
-        static void Main(string[] args)
-        {
-
-            Start start = new Start();
-            int ymap = start.choice();
-            if (ymap == 151 || ymap == 101)
-            {
-                Program prog = new Program();
-                prog.game_draw(ymap);
-            }
-            else
-            {
-                start.Game_stop();
-            }
-
-
-            //Console.ReadLine();
+           
         }
     }
 
